f2b92b70e8b9531bf1132814e3006c58bd4b61a9
alihasan28886455484-oss <alihasan28886455484@gmail.com>
2026-06-10 10:14:17 +0530

    Add files via upload
    My second project on Git bub

---
+++ araea of Rectange.py
@@ -0,0 +1,5 @@
+l=float(input('enter the length of the rectangle: '))
+b=float(input('enter the breath of the rectangle: '))
+area=l*b
+print(f'Area of Rectange With Length{l} and Breath{b} is {area}')
+
